3b33ac1287e6ec506f5fa294b0bd8eb193d3fa85
just-vy <i.am.really.really.ugly@gmail.com>
2023-12-22 12:27:08 +0700

    commit-4: Added Home, About, Projects, Contact pages; Updated Navbar
    just-vy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import {Home, About, Projects, Contact} from './pages'
 
 const App = () => {
   return (
@@ -8,10 +9,10 @@ const App = () => {
       <Router>
         <Navbar></Navbar>
         <Routes>
-          <Route path='/' element={'Home'}></Route>
-          <Route path='/about' element={'About'}></Route>
-          <Route path='/projects' element={'Projects'}></Route>
-          <Route path='/contact' element={'Contact'}></Route>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+          <Route path='/projects' element={<Projects></Projects>}></Route>
+          <Route path='/contact' element={<Contact></Contact>}></Route>
         </Routes>
       </Router>
     </main>
