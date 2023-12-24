f07741143e9e1cff59fe6fafded80eba4511227a
just-vy <i.am.really.really.ugly@gmail.com>
2023-12-24 16:30:02 +0700

    commit-6: Added Island Model
    just-vy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
+import Island from '../models/Island';
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
     </div> */}
 
 const Home = () => {
+//   const adjustIslandForScreenSize = ()=>{
+//     let screenScale = null;
+//     let screenPosition = [0, -6.5, -43];
+//     let rotation = [0.1, 4.7, 0];
+
+//     if(window.innerWidth < 768)
+//       screenScale = [0.9, 0.9, 0.9];
+//     else
+//       screenPosition = [0, -6.5, -43];
+
+//     return [screenScale, screenPosition, rotation];
+// }
+
+//   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     <section className='w-full h-screen relative'>
       <Canvas className='w-full h-screen bg-transparent' camera={{near: 0.1, far: 100}}>
@@ -14,6 +30,8 @@ const Home = () => {
           <ambientLight></ambientLight>
           <pointLight></pointLight>
           <hemisphereLight></hemisphereLight>
+          {/* <Island position={islandPosition} scale={islandScale} rotation={islandRotation}></Island> */}
+          <Island></Island>
         </Suspense>
       </Canvas>
     </section>
